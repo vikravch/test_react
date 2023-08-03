80ac15fc4c94ef6301b36a421e4ba2bd0329866e
vikravch <vitaly.kravchyk@gmail.com>
2023-08-03 19:14:31 +0300

    Test API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const getData = ()=>{
+  fetch("https://ecommerce-sport-production.up.railway.app/blog?page=0&size=6")
+      .then(response => {
+        console.log(response.ok);
+        return response.json()
+      }).then(data => {
+    console.log(data);
+  })
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick = {()=>{getData()}}>Get data</button>
       </header>
     </div>
   );
