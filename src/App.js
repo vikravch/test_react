28e40bcf4f541a99394a0618d2f21208c01338b8
vikravch <vitaly.kravchyk@gmail.com>
2023-08-03 19:27:31 +0300

    Test API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 const getData = ()=>{
-  fetch("https://ecommerce-sport-production.up.railway.app/blog?page=0&size=6")
+  /*fetch(
+      "https://ecommerce-sport-production.up.railway.app/blog?page=0&size=6",
+      {
+
+      })
       .then(response => {
         console.log(response.ok);
         return response.json()
       }).then(data => {
     console.log(data);
-  })
+  })*/
+    const raw = JSON.stringify({
+        "birthDate": "1999-01-01",
+        "name": "Name",
+        "surname": "Surname"
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        body: raw,
+    };
+
+    fetch("https://ecommerce-sport-production.up.railway.app/auth/registration",
+        requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+    /*var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Basic YWRtaW5AYWRtaW4uY29tOmFkbWlu");
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("localhost:8181/auth/login", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));*/
 }
 
 function App() {
