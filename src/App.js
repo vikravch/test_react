97eb65780d50f99193eb55030a3d5949feaf47b0
vikravch <vitaly.kravchyk@gmail.com>
2023-09-11 19:08:52 +0300

    New server req

--- src/App.js
+++ src/App.js
@@ -2,53 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 const getData = ()=>{
-  /*fetch(
-      "https://ecommerce-sport-production.up.railway.app/blog?page=0&size=6",
-      {
-
-      })
-      .then(response => {
-        console.log(response.ok);
-        return response.json()
-      }).then(data => {
-    console.log(data);
-  })*/
-    const raw = JSON.stringify({
-        "birthDate": "1999-01-01",
-        "name": "Name",
-        "surname": "Surname"
-    });
-
     var myHeaders = new Headers();
-    myHeaders.append("User-Password", "Basic dXNlckBnbWFpbC5jb206UGFzc3dvcmQx");
-    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("AccessToken", "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJFY29tbWVyY2VKV1QiLCJzdWIiOiJKV1QtQWNjZXNzIiwidXNlcm5hbWUiOiJhZG1pbkBhZG1pbi5jb20iLCJhdXRob3JpdGllcyI6IlJPTEVfQURNSU4iLCJpYXQiOjE2OTQ0NDczMjcsImV4cCI6MTY5NDQ2MTcyN30.WACl62828CAtz2Sj1schGZpuS0OGMGg944UFD4OiWwn5ulfb641vqk3BTBwhXUEg")
+    myHeaders.append("RefreshToken", "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJFY29tbWVyY2VKV1QiLCJzdWIiOiJKV1QtUmVmcmVzaCIsInVzZXJuYW1lIjoiYWRtaW5AYWRtaW4uY29tIiwiYXV0aG9yaXRpZXMiOiJST0xFX0FETUlOIiwiaWF0IjoxNjk0NDQ3MzI3LCJleHAiOjE2OTQ0NzYxMjd9.0TZwiD9VmvjEx3SQko1kDfsWcq-b8eL10X68ecRB5m4tCiSa6Vv2Ct3n7MVvsht1")
 
-    const requestOptions = {
-        method: 'POST',
+    var requestOptions = {
+        method: 'GET',
         headers: myHeaders,
-        body: raw,
         redirect: 'follow'
     };
 
-    fetch("https://ecommerce2-sport-production.up.railway.app/auth/registration",
-        requestOptions)
+
+    fetch("https://ecommerce3-sport-production.up.railway.app/profile", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
-
-    /*var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic YWRtaW5AYWRtaW4uY29tOmFkbWlu");
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    fetch("localhost:8181/auth/login", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));*/
 }
 
 function App() {
